095eb414894b81a4000608ef5b7c2ce7726da68d
Ugo <ugo.prenat@gmail.com>
2021-04-29 22:28:48 +0200

    good

--- wp-content/themes/LJPTtheme/single-post.php
+++ wp-content/themes/LJPTtheme/single-post.php
@@ -2,15 +2,15 @@
 
 <?php if(have_posts()): ?>
     <?php while(have_posts()): the_post() ?>
-        <div class="article-section">
-            <h1><?php the_title() ?></h1>
-            <p class="author-and-date"><?php the_author() ?> le <?php echo get_the_date() ?></p>
-            <p><?php the_content() ?></p>
+        <div class="single-post-container">
+            <div class="titles">
+                <h1><?php the_title() ?></h1>
+                <p class="author-and-date"><?php the_author() ?> le <?= get_the_date() ?></p>
+            </div>
+            <div class="single-post-content"><?= get_the_content() ?></div>
         </div>
 
     <?php endwhile ?>
-<?php else: ?>
-    <p>Aucun article</p>
-<?php endif; ?>
+<?php else: endif; ?>
 
 <?php get_footer() ?>
